e69f752a366acd90a95146bf17bb07cc5c70f5e8
Sergey <s.miller@list.ru>
2024-07-02 17:17:16 +0700

    1. Connection string for PostgreSQL is added in appsettings.json configuration 2. new attributes is added on entities

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -1,16 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities.Models;
 
+[Table("categories")]
 public class Category
 {
+    [Column("category_id")]
     public Guid Id { get; set; }
 
+    [Column("category_title")]
     [Required]
     [MinLength(5, ErrorMessage = "Минимальное число символов для наименования категории - 5.")]
     [StringLength(100, ErrorMessage = "Максимальное число символов для наименования категории - 100.")]
     public string Title { get; set; } = string.Empty;
 
+    [Column("description")]
     [StringLength(200, ErrorMessage = "Максимальная число символов для поля описание категории - 200")]
     public string? Description { get; set; }
     public ICollection<TaskItem>? Tasks { get; set; }
--- Entities/Models/TaskItem.cs
+++ Entities/Models/TaskItem.cs
@@ -4,19 +4,26 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities.Models;
 
+[Table("tasks")]
 public class TaskItem
 {
+    [Column("task_id")]
     public Guid Id { get; set; }
 
+    [Column("task_title")]
     [Required(ErrorMessage = "Необходимо указать название задачи.")]
     [MinLength(5, ErrorMessage = "Минимальное число символов для наименования задачи - 5.")]
     [StringLength(100, ErrorMessage = "Максимальное число символов для наименования задачи - 100.")]
     public string Title { get; set; } = string.Empty;
 
+    [Column("description")]
     [StringLength(200, ErrorMessage = "Максимальная число символов для поля описание задачи - 200")]
     public string? Description { get; set; }
+
+    [Column("priority")]
     public Priority PriorityTask { get; set; }
-    
+
+    [Column("category_id")]
     [ForeignKey(nameof(Category))]
     public Guid CategoryId { get; set; }
     public Category? Category { get; set; }
